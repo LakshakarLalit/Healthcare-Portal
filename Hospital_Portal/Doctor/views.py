8085d9fafa892285f3a9bf3b7f4eb8a1ae936829
LakshakarLalit <l.l12052002@gmail.com>
2026-05-28 14:29:04 +0530

    define doctor views, get, put, post, delete

--- Hospital_Portal/Doctor/views.py
+++ Hospital_Portal/Doctor/views.py
@@ -2,24 +2,53 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .models import Doctor
+from .serializer import DoctorSerializer
+
 # Create your views here.
 
 @api_view(['GET'])
 def getAll(request):
-    return Response({'message': "Success", })
+    allDoctor = Doctor.objects.all()
+    data = DoctorSerializer(allDoctor, many=True).data
+    return Response({'message': "Success", "data": data})
 
 @api_view(['GET'])
 def getById(request, id):
-    return
+    try:
+        doctor = Doctor.objects.get(id=id)
+        data = DoctorSerializer(doctor).data
+        return Response({"message": "Success", "data": data})
+    
+    except Doctor.DoesNotExist:
+        return Response({"message": "Not Found", "data": {}})
 
 @api_view(['POST'])
 def addDoctor(request):
-    return Response({"message": "Successfully Added"})
+    newDoctor = request.data
+    doctor = DoctorSerializer(data = newDoctor)
+    if doctor.is_valid():
+        doctor.save()
+        return Response({"message": "Successfully Added", "data": doctor.data})
+    return Response({"message": "Error", "data": doctor.errors})
 
 @api_view(['PUT'])
 def updateDoctor(request, id):
-    return Response({'message': 'updated'})
+    try:
+        doctor = Doctor.objects.get(id=id)
+        serializer = DoctorSerializer(doctor, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "Success", "data": serializer.data})
+        return Response({"message": "Error", "data": serializer.errors})
+    except Doctor.DoesNotExist:
+        return Response({"message": "Error", "data": "Doctor not found"})
 
 @api_view(['DELETE'])
 def deleteDoctor(request, id):
-    return Response({'message': 'Deleted'})
+    try:
+        doctor = Doctor.objects.get(id=id)
+        doctor.delete()
+        return Response({"message": "Success", "data": {}})
+    except Doctor.DoesNotExist:
+        return Response({"message": "Error", "data": "Doctor not found"})
